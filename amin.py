f89e3b3a5b8e25a9cd2bc4fc9566a06dd7144c18
khadem-al-mahdi <71723210+khodekhadem@users.noreply.github.com>
2023-01-26 15:30:11 +0330

    Update amin.py

--- amin.py
+++ amin.py
@@ -1,9 +1,42 @@
-from flask import Flask
 import os
 import subprocess
-def sysout( command ):
-    output = subprocess.check_output( command , shell=True)
-    output = output.decode("utf-8")
-    print (output)
-    return "hello"
-sysout("mysql -e show variable")
+from flask import Flask
+import mysql.connector
+
+
+
+
+
+def convertTuple(tup):
+        # initialize an empty string
+    str = ''
+    for item in tup:
+        str = str + item
+    return str
+ 
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="ansible",
+  password="password",
+  database="mysql"
+)
+
+mycursor = mydb.cursor()
+
+#mycursor.execute("SHOW TABLES")
+mycursor.execute("show variables like 'innodb%';")
+mystr = ""
+for x in mycursor:
+  mystr = mystr +convertTuple(x) + "\n" 
+#print(mystr)
+
+
+
+
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return mystr
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+
